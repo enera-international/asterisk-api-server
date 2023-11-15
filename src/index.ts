e5e348d31887ccc27a15d68fc3e7da048a805142
enera-international <anders.westberg@enera.com>
2023-11-15 11:14:04 +0100

    First version

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { EneraAsteriskApiServer } from "./api";
+import { Server } from "./server";
+
+const eneraAsteriskApiServer = new EneraAsteriskApiServer()
+new Server(eneraAsteriskApiServer)
